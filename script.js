6d49afddaeb37431a833a33782ac5450ba5fcfef
sidhantkaushal <kaushalsidhant434@gmail.com>
2022-11-12 17:32:17 +0530

    added solution

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+for(var i=0;i<names.length;i++){
+  if(names[i][0]=='J' || names[i][0]=='j'){
+    console.log("Goodbye"+names[i]);
+  }
+  else{
+    console.log("Hello"+names[i]);
+  }
+}
